72fcf429679c635aa2418c6c45b2c806621d4b2f
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-05-09 20:52:51 -0400

    Add max workers limit option to config

--- src/Drivers/SearchConsoleDriver.php
+++ src/Drivers/SearchConsoleDriver.php
@@ -383,6 +383,9 @@
             if (isset($newData['granular_sync'])) {
                 $chanCfg['granular_sync'] = filter_var($newData['granular_sync'], FILTER_VALIDATE_BOOLEAN);
             }
+            if (isset($newData['max_workers'])) {
+                $chanCfg['max_workers'] = (int)$newData['max_workers'];
+            }
 
             // Synthetic generation toggle
             if (isset($featureToggles['calculate_synthetics'])) {
@@ -1288,6 +1291,7 @@
             $ui['gsc_cron_recent_minute'] = $channelConfig['cron_recent_minute'] ?? 0;
             $ui['gsc_calculate_synthetics'] = filter_var($channelConfig['calculate_synthetics'] ?? true, FILTER_VALIDATE_BOOLEAN);
             $ui['gsc_granular_sync'] = $channelConfig['granular_sync'] ?? false;
+            $ui['gsc_max_workers'] = $channelConfig['max_workers'] ?? 3;
 
             $ui['gsc'] = [];
             foreach (($channelConfig['sites'] ?? []) as $site) {
